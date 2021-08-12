04909ebd28b009dd29785c51e13bc31c36852c2a
omo-y <46962034+omo-y@users.noreply.github.com>
2021-08-12 13:16:30 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# customfook
+# custumfook
 Created with CodeSandbox
---
+++ src/hooks/useAllUsers.ts
@@ -0,0 +1,33 @@
+/*全ユーザー情報の返却*/
+import axios from "axios";
+import { useState } from "react";
+import { UserProfile } from "../types/UserProfile";
+import { User } from "../types/api/user";
+
+export const useAllUsers = () => {
+  const [userProfiles, setuserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setloading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const getUsers = () => {
+    setloading(true);
+    setError(false);
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setuserProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setloading(false);
+      });
+  };
+};
